23e769c8648b7803f67bc60d896a0cb837db28fd
Johndel Villanueva <Johndel0307@gmail.com>
2025-07-13 13:01:40 +0800

    Update Commit -laptop

--- src/controllers/auth/routes.ts
+++ src/controllers/auth/routes.ts
@@ -1,7 +1,6 @@
 import { Hono } from "hono";
 import {
   userLoginController,
-  createUserController,
   createEmployerController,
   getUserById,
 } from "./index.js";
@@ -10,7 +9,6 @@ import { authMiddleware } from "../../utils/auth.js";
 const auth = new Hono()
   .get('/users/:id', authMiddleware, getUserById)
   .post("/login", userLoginController)
-  .post("/create", createUserController)
   .post("/createEmployer", createEmployerController)
 
 export default auth;
